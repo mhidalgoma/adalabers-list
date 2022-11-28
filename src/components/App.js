2120bedf85166d34119976e27b3009ed220f44d5
Monica Hidalgo Martin <monicahidalgomartin@gmail.com>
2022-11-28 09:14:15 +0100

    Render all adalaber on html and create form with inputs (no function yet)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,45 @@
 
 import '../styles/App.css';
+import { useState } from 'react';
+import adalabers from "../data/adalabers.json";
+
 
 function App() {
+const [data, setData] = useState (adalabers);
+
+const tableData = data.results.map ((adalaber)=>{
+return <tr key={adalaber.id}>
+<td>{adalaber.name}</td>
+<td>{adalaber.counselor}</td>
+<td>{adalaber.speciality}</td>
+</tr>
+})
+
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <header>
+      <h1>Adalabers</h1>
+      </header>
+      
+      <table className="table">
+        <thead><tr>
+          <th>Nombre</th>
+          <th>Tutora</th>
+          <th>Especialidad</th>
+        </tr></thead>
+        <tbody>
+          {tableData}
+        </tbody>
+      </table>
+      <form>
+        <label htmlFor="firstName">Nombre</label>
+        <input type="text" id='firstName' name='firstName' />
+        <label htmlFor="counselor">Tutora</label>
+        <input type="text" id='counselor' name='counselor' />
+        <label htmlFor="speciality">Especialidad</label>
+        <input type="text" id='speciality' name='speciality' /> 
+        <button>Añadir una nueva Adalaber</button>
+      </form>
     </div>
   );
 }
